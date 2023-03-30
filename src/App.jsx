7ac7b79b9337fcbe54224f3cfb52612004dcf0fa
hackerboy09 <isc1930319@gmail.com>
2023-03-30 09:59:22 -0600

    Configuracion inicial

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+function App() {
+
+  return (
+    <div className="container text-center">
+      <div className="row">
+        <div className="col">
+        <h1>Shopping List</h1>
+        <hr />
+        </div>
+      </div>
+      <div className="row">
+      <div className="col">C</div>
+      <div className="col-8"> Nombre Item</div>
+      <div className="col-3"> Botone </div>
+      </div>
+      <div className="row">
+      <div className="col">C</div>
+      <div className="col-8"> Nombre Item</div>
+      <div className="col-3"> Botone </div>
+      </div>
+      <div className="row">
+      <div className="col">C</div>
+      <div className="col-8"> Nombre Item</div>
+      <div className="col-3"> Botone </div>
+      </div>
+  </div>
+  )
+
+}
+
+export default App
